62fafeebf22920c1f9d50248e19db268ae01053f
Robson Cruz <robs.cruz@gmail.com>
2017-11-15 13:49:22 -0200

    Execute from Script

--- app-instances.tf
+++ app-instances.tf
@@ -16,50 +16,15 @@ resource "aws_instance" "master" {
     user = "ubuntu"
     private_key = "${file("~/.ssh/longevo-tech.pem")}"
   }
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update",
-      "sudo apt-get install git",
-      "sudo apt-get install -y apt-transport-https ca-certificates",
-      "sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D",
-      "sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'",
-      "sudo apt-get update",
-      "sudo apt-cache policy docker-engine",
-      "sudo apt-get install -y docker-engine wget zip unzip htop",
-      "sudo docker swarm init",
-      "sudo docker swarm join-token --quiet worker > /home/ubuntu/token",
-      "sudo curl -o /usr/local/bin/docker-compose -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose-$(uname -s)-$(uname -m)",
-      "sudo chmod +x /usr/local/bin/docker-compose",
-      "sudo mkdir -p /${var.path_project} && sudo chown -Rf root:ubuntu /${var.path_project} && sudo chmod 775 -Rf /${var.path_project}",
-      "cd /${var.path_project} && wget https://github.com/robsonscruz/env-urbem/archive/master.zip",
-      "cd /${var.path_project} && unzip master.zip && mv env-urbem-master env-urbem && rm master.zip",
-      "cd /${var.path_project}/env-urbem && mv volumes.yml.dist volumes.yml",
-      "sudo usermod -aG docker $USER",
-      "cd /${var.path_project} && wget https://github.com/tilongevo/urbem3.0/archive/v201710251800.zip",
-      "cd /${var.path_project} && unzip v201710251800.zip",
-      "cd /${var.path_project}/env-urbem/www && rm -rf urbem-prod",
-      "cd /${var.path_project} && mv urbem3.0-201710251800 /${var.path_project}/env-urbem/www/urbem-prod && rm v201710251800.zip",
-      "cd /${var.path_project} && wget https://github.com/tilongevo/banco-zerado-urbem3.0/archive/master.zip",
-      "cd /${var.path_project} && unzip master.zip",
-      "cd /${var.path_project} && mv banco-zerado-urbem3.0-master /${var.path_project}/env-urbem/www/urbem-prod/banco-zerado-urbem3.0 && rm banco-zerado-urbem3.0-master.zip",
-      "sudo mkdir -p /${var.path_project}/env-urbem/www/urbem-prod/var",
-      "sudo chmod 777 -Rf /${var.path_project}/env-urbem/www/urbem-prod/var",
-      "cd /${var.path_project}/env-urbem && sudo docker-compose build && sudo docker-compose up -d && sudo docker-compose up -d --force-recreate",
-      "sudo docker exec envurbem_web-urbem_1 php /srv/web/urbem/vendor/sensio/distribution-bundle/Resources/bin/build_bootstrap.php",
-      "sudo docker exec envurbem_web-urbem_1 php /srv/web/urbem/bin/console cache:clear --no-warmup --env=prod",
-      "sudo docker exec envurbem_web-urbem_1 php /srv/web/urbem/bin/console cache:warmup --env=prod",
-      "sudo docker exec envurbem_web-urbem_1 /srv/web/urbem/bin/console assetic:dump --no-debug",
-      "sudo docker exec envurbem_web-urbem_1 /srv/web/urbem/bin/console assets:install /srv/web/urbem/web --symlink --relative -vvv",
-      "sudo chmod 777 -Rf /${var.path_project}/env-urbem/www/urbem-prod/var/*",
-      "sudo chmod 777 -Rf /${var.path_project}/env-urbem/www/storage",
-      "sudo docker exec envurbem_web-urbem_1 touch /var/www/storage/prefeitura.yml",
-      "sudo chmod 777 -Rf /${var.path_project}/env-urbem/www/storage/*",
-      "cd /${var.path_project}/env-urbem && sudo docker-compose build && sudo docker-compose up -d && sudo docker-compose up -d --force-recreate",
-    ]
-  }
   provisioner "file" {
     source = "proj"
     destination = "/home/ubuntu/"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chmod a+x /home/ubuntu/proj/install-urbem.sh",
+      "sudo /home/ubuntu/proj/install-urbem.sh"
+    ]
   }
   tags = {
     Name = "${var.instance_name}-master"
---
+++ proj/install-urbem.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+ROOT_PATH_PROJECT="/datacenter"
+# ROOT_PATH_PROJECT="/home/rcruz/Projetos"
+URL_ENV_URBEM_DOCKER="https://github.com/robsonscruz/env-urbem/archive/master.zip"
+VERSION_URBEM="201711151345"
+URL_PROJECT_URBEM="https://github.com/tilongevo/urbem3.0/archive/v${VERSION_URBEM}.zip"
+URL_PROJECT_TRANSPARENCIA="https://github.com/tilongevo/urbem-transparencia/archive/v1.2.1.zip"
+URL_BANCO_DADOS="https://github.com/tilongevo/banco-zerado-urbem3.0/archive/master.zip"
+
+installDocker() {
+    echo "Instalando Docker..."
+    apt-get update && \
+    apt-get install git && \
+    apt-get install -y apt-transport-https ca-certificates && \
+    apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D && \
+    apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' && \
+    apt-get update && \
+    apt-cache policy docker-engine && \
+    apt-get install -y docker-engine wget zip unzip htop && \
+    docker swarm init && \
+    docker swarm join-token --quiet worker > /home/ubuntu/token && \
+    curl -o /usr/local/bin/docker-compose -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose-$(uname -s)-$(uname -m) && \
+    chmod +x /usr/local/bin/docker-compose && \
+
+    echo "Instalacao do Docker finalizada."
+}
+
+permissionUserLoggedDocker() {
+    echo "Aplicando permissao ao usuario executar o Docker"
+    usermod -aG docker $USER
+}
+
+createRootPathEnvDocker() {
+    echo "Criando pasta padrao para execucao do projeto"
+    mkdir -p /${ROOT_PATH_PROJECT} && \
+    chown -Rf root:ubuntu /${ROOT_PATH_PROJECT} && \
+    chmod 775 -Rf /${ROOT_PATH_PROJECT}
+}
+
+downloadEnvUrbem() {
+    echo "Executando download do EnvUrbem - Docker"
+    cd /${ROOT_PATH_PROJECT} && wget ${URL_ENV_URBEM_DOCKER} && \
+    cd /${ROOT_PATH_PROJECT} && unzip master.zip && mv env-urbem-master env-urbem && rm master.zip && \
+    cd /${ROOT_PATH_PROJECT}/env-urbem && mv volumes.yml.dist volumes.yml
+}
+
+downloadUrbem() {
+    echo "Executando download do Urbem 3.0"
+    cd /${ROOT_PATH_PROJECT} && wget ${URL_PROJECT_URBEM} && \
+    cd /${ROOT_PATH_PROJECT} && unzip v${VERSION_URBEM}.zip && \
+    cd /${ROOT_PATH_PROJECT}/env-urbem/www && rm -rf urbem-prod && \
+    cd /${ROOT_PATH_PROJECT} && mv urbem3.0-${VERSION_URBEM} /${ROOT_PATH_PROJECT}/env-urbem/www/urbem-prod && rm v${VERSION_URBEM}.zip
+}
+
+downloadPortalDaTransparencia() {
+    echo "Executando download do Portal da Transparencia"
+    cd /${ROOT_PATH_PROJECT} && wget ${URL_PROJECT_TRANSPARENCIA} && \
+    cd /${ROOT_PATH_PROJECT} && unzip v1.2.1.zip && \
+    cd /${ROOT_PATH_PROJECT}/env-urbem/www && rm -rf transparencia-prod && \
+    cd /${ROOT_PATH_PROJECT} && mv urbem-transparencia-1.2.1 /${ROOT_PATH_PROJECT}/env-urbem/www/transparencia-prod && rm v1.2.1.zip && \
+    chmod 777 -Rf /${ROOT_PATH_PROJECT}/env-urbem/www/transparencia-prod/uploads && \
+    chmod 777 -Rf /${ROOT_PATH_PROJECT}/env-urbem/www/transparencia-prod/tmp
+}
+
+downloadDatabases() {
+    echo "Executando download dos bancos necessarios para criacao de todo parque Longevo/Urbem"
+    cd /${ROOT_PATH_PROJECT} && wget ${URL_BANCO_DADOS} && \
+    cd /${ROOT_PATH_PROJECT} && unzip master.zip && \
+    cd /${ROOT_PATH_PROJECT} && mv banco-zerado-urbem3.0-master /${ROOT_PATH_PROJECT}/env-urbem/www/urbem-prod/banco-zerado-urbem3.0 && rm banco-zerado-urbem3.0-master.zip
+}
+
+createPathTempUrbem() {
+    echo "Criando pastas temporarias para execucao do Urbem - cache, logs, sessions"
+    mkdir -p /${ROOT_PATH_PROJECT}/env-urbem/www/urbem-prod/var
+    chmod 777 -Rf /${ROOT_PATH_PROJECT}/env-urbem/www/urbem-prod/var
+}
+
+reinitializeDocker() {
+    echo "Reiniciando Docker"
+    cd /${ROOT_PATH_PROJECT}/env-urbem && sudo docker-compose build && sudo docker-compose up -d && sudo docker-compose up -d --force-recreate
+}
+
+initializeUrbem() {
+    echo "Inicializando Urbem 3.0"
+    docker exec envurbem_web-urbem_1 php /srv/web/urbem/vendor/sensio/distribution-bundle/Resources/bin/build_bootstrap.php && \
+    docker exec envurbem_web-urbem_1 php /srv/web/urbem/bin/console cache:clear --no-warmup --env=prod && \
+    docker exec envurbem_web-urbem_1 php /srv/web/urbem/bin/console cache:warmup --env=prod && \
+    docker exec envurbem_web-urbem_1 /srv/web/urbem/bin/console assetic:dump --no-debug && \
+    docker exec envurbem_web-urbem_1 /srv/web/urbem/bin/console assets:install /srv/web/urbem/web --symlink --relative -vvv && \
+    chmod 777 -Rf /${ROOT_PATH_PROJECT}/env-urbem/www/urbem-prod/var/* && \
+    chmod 777 -Rf /${ROOT_PATH_PROJECT}/env-urbem/www/storage && \
+    docker exec envurbem_web-urbem_1 touch /var/www/storage/prefeitura.yml && \
+    chmod 777 -Rf /${ROOT_PATH_PROJECT}/env-urbem/www/storage/*
+}
+
+permissionPathStorage() {
+    chmod 777 -Rf /${ROOT_PATH_PROJECT}/env-urbem/www/urbem-prod/var && \
+    chmod 777 -Rf /${ROOT_PATH_PROJECT}/env-urbem/www/urbem-prod/var/* && \
+    chmod 777 -Rf /${ROOT_PATH_PROJECT}/env-urbem/www/storage && \
+    chmod 777 -Rf /${ROOT_PATH_PROJECT}/env-urbem/www/storage/*
+}
+
+#PROD
+installDocker
+permissionUserLoggedDocker
+createRootPathEnvDocker
+downloadEnvUrbem
+
+# DEV
+downloadUrbem
+downloadPortalDaTransparencia
+downloadDatabases
+createPathTempUrbem
+reinitializeDocker
+initializeUrbem
+reinitializeDocker
+permissionUserLoggedDocker
+permissionPathStorage
